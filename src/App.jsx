a2e62e125bc0d361ef693ad0e29d6cdc7930d033
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-03-20 15:04:32 +0700

    feat(app): add starter template content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import reactLogo from './assets/react.svg';
+import reactVsVanillaJs from './assets/react-vs-vanilla-js.jpg';
+
 export default function App() {
   return (
-    <main className='h-dvh content-center border bg-zinc-700'>
-      <h1 className='text-center text-3xl text-white'>Hello, world!</h1>
+    <main className='grid h-dvh place-items-center content-center gap-8 border bg-zinc-700'>
+      <div className='flex gap-4'>
+        <h1 className='text-center text-6xl text-white'>Hello, React!</h1>
+        <img src={reactLogo} alt='React Logo' className='size-16' />
+      </div>
+      <img
+        src={reactVsVanillaJs}
+        alt='We are moving to React now, bye bye vanilla JavaScript...'
+        className='rounded-xl shadow-md'
+      />
+      <p className='text-center text-xl text-white'>
+        It's time to move on from
+        <span className='font-semibold text-yellow-400'>
+          {' '}
+          HTML + Vanilla JS{' '}
+        </span>
+        to
+        <span className='font-semibold text-sky-400'> React with JSX</span>
+      </p>
     </main>
   );
 }
